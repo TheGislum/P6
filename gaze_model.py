7fc484677e58aaabed24dd704d07c744effd6233
TheGislum <gislum.casper@gmail.com>
2022-04-13 19:22:17 +0200

    improved model

--- gaze_model.py
+++ gaze_model.py
@@ -1,9 +1,73 @@
 from torch import nn
 
-class annet(nn.Module):
+class annetV2(nn.Module):
 
     def __init__(self, device, in_channels=3):
-        super(annet, self).__init__()
+        super(annetV2, self).__init__()
+
+        conv_model = nn.Sequential(
+            #64 -> 32 BLOCK
+            #CHANNEL SIZE 128
+            nn.Conv2d(in_channels,128, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #32 -> 16 BLOCK
+            #CHANNEL SIZE 256
+            nn.Conv2d(128,256, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #16 -> 8 BLOCK
+            #CHANNEL SIZE 512
+            nn.Conv2d(256,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #8 -> 4 BLOCK
+            #CHANNEL SIZE 512
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+        )
+
+        # FCN og X,Y output layer
+        self.fcn_model = nn.Sequential(
+            conv_model,
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,2), #2 = OUTPUT COORDINATES (x,y screen space)
+            nn.Tanh(),
+        ).to(device)
+
+    def forward(self, input):
+        output = self.full_model(input)
+        return output
+
+
+class annetV1(nn.Module):
+
+    def __init__(self, device, in_channels=3):
+        super(annetV1, self).__init__()
 
         conv_model = nn.Sequential(
             #64 -> 32 BLOCK
