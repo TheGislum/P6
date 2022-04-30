968c7c6a1cb51ce436b00dab9b3889a3671750df
TheGislum <gislum.casper@gmail.com>
2022-04-30 15:52:47 +0200

    headpose added to model (V3)

--- gaze_model.py
+++ gaze_model.py
@@ -1,5 +1,70 @@
 from torch import nn
 
+class annetV3(nn.Module):
+
+    def __init__(self, device, in_channels=2):
+        super(annetV3, self).__init__()
+
+        self.conv_model = nn.Sequential(
+            #64 -> 32 BLOCK
+            #CHANNEL SIZE 128
+            nn.Conv2d(in_channels,128, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #32 -> 16 BLOCK
+            #CHANNEL SIZE 256
+            nn.Conv2d(128,256, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #16 -> 8 BLOCK
+            #CHANNEL SIZE 512
+            nn.Conv2d(256,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            #8 -> 4 BLOCK
+            #CHANNEL SIZE 512
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, padding=1), #Normal convolution layer
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512, 3, stride=2, padding=1), #Halverende Layer
+            nn.ReLU(inplace=True),
+            
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+        ).to(device)
+
+        # FCN og X,Y output layer
+        self.FCN_model = nn.Sequential(
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512,2), #2 = OUTPUT COORDINATES (x,y screen space)
+            nn.Tanh(),
+        ).to(device)
+        
+        self.headpose_layer = nn.Linear(6,512).to(device)
+
+    def forward(self, image, headpose):
+        output = self.conv_model(image) + self.headpose_layer(headpose)
+        output = self.fcn(output)
+        return output
+
 class annetV2(nn.Module):
 
     def __init__(self, device, in_channels=3):
